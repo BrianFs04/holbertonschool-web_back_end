5acb732588b5e600036cdd8c90042bf047f3b832
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-03-16 14:48:55 -0500

    [Feature]: Tasks

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,5 +6,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> Task:
+    """Tasks"""
     task = create_task(wait_random(max_delay))
     return task
