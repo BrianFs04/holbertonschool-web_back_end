5ef0abd1f0d4c8aabe93f8e20ec7999f4a5f0f94
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-03-17 13:29:10 -0500

    [Feature]: Async comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""async_comprehensions"""
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Coroutine that collects 10 random numbers using an
+    async comprehensing over async_generator,
+    then return the 10 random numbers."""
+    return [i async for i in async_generator()]
