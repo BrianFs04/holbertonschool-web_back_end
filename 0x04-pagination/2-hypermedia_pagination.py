cdf6a7548edda7c984e20e9bcf1e8f813cd93762
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-03-24 11:59:29 -0500

    [Feature]: Hypermedia pagination

---
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+Simple pagination
+"""
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters."""
+    end_index = page * page_size
+    start_index = (page - 1) * page_size
+    res = (start_index, end_index)
+    return res
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns the appropriate page of the dataset
+        """
+        assert all(isinstance(i, int) for i in [page, page_size]) and all(
+            i > 0 for i in [page, page_size])
+
+        dataset = self.dataset()
+
+        if dataset is not None:
+            idx_rng = index_range(page, page_size)
+            rows = dataset[idx_rng[0]:idx_rng[1]]
+            return rows
+
+        return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a dictionary containing the following key-value pairs
+        page_size: the length of the returned dataset page
+        page: the current page number
+        data: the dataset page (equivalent to return from previous task)
+        next_page: number of the next page, None if no next page
+        prev_page: number of the previous page, None if no previous page
+        total_pages: the total number of pages in the dataset as an integer
+        """
+        dataset = self.dataset()
+        total_pages = math.ceil(len(dataset) / page_size)
+        data = self.get_page(page, page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+        data_dict = {
+            "page_size": page_size if data else 0,
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
+        return data_dict
