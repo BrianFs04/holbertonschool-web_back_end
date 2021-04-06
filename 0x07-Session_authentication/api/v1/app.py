6739e2ef9e3983e15ad785028c4c6014bdb75d98
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-04-06 16:27:35 -0500

    [Feature]: Before request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -29,11 +29,13 @@ def before_request() -> None:
     """Filters each request"""
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, excluded_paths):
             request.current_user = auth.current_user(request)
-            if auth.authorization_header(request) is None:
+            if not auth.authorization_header(request) and\
+                    not auth.session_cookie(request):
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
