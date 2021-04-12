3ea4d7efa816c3b93c8008a7f927980042f10b04
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-04-12 10:36:39 -0500

    [Fix]: Docstrings

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """
-DB module
+DB module, create, find and update user
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-
 from user import Base, User
 
 
