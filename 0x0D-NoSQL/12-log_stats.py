33afd90c39475cb70d1f53d3c8720c4378fddafb
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-05-04 17:23:03 -0500

    [Feature]: Data sample

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """ log_stats module """
 from pymongo import MongoClient
+client = MongoClient()
+collection = client.logs.nginx
+logs = collection.count_documents({})
+status = collection.count_documents({"method": "GET", "path": "/status"})
 
 if __name__ == "__main__":
-    client = MongoClient()
-    collection = client.logs.nginx
-    logs = collection.count_documents({})
+
     print(f"{logs} logs")
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
@@ -14,5 +16,4 @@ if __name__ == "__main__":
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status = collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status} status check")
