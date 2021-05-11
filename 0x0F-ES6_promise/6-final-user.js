a0085dfe2ff6f6c4c110dfa844866d80246b80e6
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-05-11 16:28:58 -0500

    [Fix]: Format

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -3,5 +3,14 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises);
+  const res = [];
+  return Promise.allSettled(promises)
+    .then((results) => {
+      results.map((result) => (
+        res.push({
+          status: result.status,
+          value: result.status === 'rejected' ? result.reason.toString() : result.value,
+        })));
+      return res;
+    });
 }
