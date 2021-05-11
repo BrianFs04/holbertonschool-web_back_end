fe0d6b3b34f17d3d428101d85109e71cee1641a1
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-05-11 16:32:40 -0500

    [Fix]: Return the array

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -11,5 +11,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
           status,
           value: status === 'rejected' ? reason.toString() : value,
         })));
+      return res;
     });
 }
