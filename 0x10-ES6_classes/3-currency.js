7ad9bedadf17ffd25e6395dc9299fc42698cea42
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-05-13 15:42:00 -0500

    [Feature]: Methods

---
+++ 0x10-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    this._code = newCode;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
