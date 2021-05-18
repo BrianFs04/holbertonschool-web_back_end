7f39622df3fb295dba9e86251c025eb49ebefaf8
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-05-18 12:50:45 -0500

    [Feature]: Clean set

---
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+const cleanSet = (set, startString) => {
+  let res = '';
+  if (startString) {
+    set.forEach((i) => {
+      if (i.startsWith(startString)) res += `${i.slice(startString.length)}-`;
+    });
+  }
+  return res.slice(0, res.length - 1);
+};
+
+export default cleanSet;
