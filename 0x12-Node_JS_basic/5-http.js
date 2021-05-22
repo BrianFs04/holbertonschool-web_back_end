9caaa8701150450a5051d4a4effb656973f35ecb
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-05-22 14:29:14 -0500

    [Fix]: Conditions

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -2,13 +2,16 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
     try {
       const data = await countStudents(process.argv[2]);
-      res.end(`This is the list of our students\n${data}`);
+      res.end(data);
     } catch (err) {
       res.end(err.message);
     }
