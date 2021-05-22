bf282e21769d378d6dd8761e28e4156686d5c316
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-05-22 14:33:34 -0500

    [Fix]: Conditions

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -2,8 +2,7 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   }
