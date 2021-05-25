0a5889eaa8f1511bc2bf037eb443d1842685ff32
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-05-25 13:33:08 -0500

    [Fix]: Timeout of 2000ms exceeded

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -1,11 +1,8 @@
 const fs = require('fs');
 
-const countStudents = (path) => new Promise((resolve, reject) => {
-  fs.readFile(path, 'UTF-8', (err, data) => {
-    if (err) {
-      reject(Error('Cannot load the database'));
-      return;
-    }
+const countStudents = async (path) => {
+  try {
+    const data = await fs.promises.readFile(path, { encoding: 'utf8' });
     const columns = data.split('\n').slice(1, data.length);
     let count = 0;
     const fields = {};
@@ -16,17 +13,20 @@ const countStudents = (path) => new Promise((resolve, reject) => {
       if (!fields[row[3]] && row[3]) {
         fields[row[3]] = [];
       }
-      if (row[0]) {
-        fields[row[3]].push(row[0]);
-      }
+      if (row[0]) fields[row[3]].push(row[0]);
     }
+    console.log(`Number of students: ${count}`);
     msg.push(`Number of students: ${count}`);
     for (const [key, values] of Object.entries(fields)) {
-      msg.push(`Number of students in ${key}: ${values.length}. List: ${values.join(', ')}`);
+      if (key) {
+        console.log(`Number of students in ${key}: ${values.length}. List: ${values.join(', ')}`);
+        msg.push(`Number of students in ${key}: ${values.length}. List: ${values.join(', ')}`);
+      }
     }
-    console.log(msg.join('\n'));
-    resolve(msg.join('\n'));
-  });
-});
+    return msg.join('\n');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
 
 module.exports = countStudents;
