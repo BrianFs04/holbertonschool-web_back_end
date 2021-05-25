5ca6cb290a2fdb3a0f21df2158ea6f04d3e9f704
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-05-25 13:40:22 -0500

    [Fix]: Split

--- 0x12-Node_JS_basic/full_server/utils.js
+++ 0x12-Node_JS_basic/full_server/utils.js
@@ -1,24 +1,21 @@
 import fs from 'fs';
 
-const readDatabase = (file) => new Promise((resolve, reject) => {
-  fs.readFile(file, 'UTF-8', (err, data) => {
-    if (err) {
-      reject(Error('Cannot load the database'));
-      return;
-    }
-    const columns = data.split('\r\n').slice(1, data.length);
+const readDatabase = async (file) => {
+  try {
+    const data = await fs.promises.readFile(file, { encoding: 'utf8' });
+    const columns = data.split('\n').slice(1, data.length);
     const fields = {};
     for (const rows of columns) {
       const row = rows.split(',');
       if (!fields[row[3]] && row[3]) {
         fields[row[3]] = [];
       }
-      if (row[0]) {
-        fields[row[3]].push(row[0]);
-      }
+      if (row[0]) fields[row[3]].push(row[0]);
     }
-    resolve(fields);
-  });
-});
+    return fields;
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
 
 export default readDatabase;
