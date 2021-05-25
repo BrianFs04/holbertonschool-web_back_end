40d71418e181ada23adb1a0ece2e5fe7bdc47376
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-05-25 12:46:49 -0500

    [Fix]: Split

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -2,21 +2,21 @@ const fs = require('fs');
 
 const countStudents = (path) => {
   try {
-    const data = fs.readFileSync(path, { encoding: 'utf8' }).split('\r\n');
+    const data = fs.readFileSync(path, { encoding: 'utf8' }).split('\n');
     const columns = data.slice(1, data.length);
     let count = 0;
     const fields = {};
     for (const rows of columns) {
       if (rows) count += 1;
       const row = rows.split(',');
-      if (!fields[row[3]]) {
+      if (!fields[row[3]] && row[3]) {
         fields[row[3]] = [];
       }
-      fields[row[3]].push(row[0]);
+      if (row[0]) fields[row[3]].push(row[0]);
     }
     console.log(`Number of students: ${count}`);
     for (const [key, values] of Object.entries(fields)) {
-      if (key !== 'undefined') {
+      if (key) {
         console.log(`Number of students in ${key}: ${values.length}. List: ${values.join(', ')}`);
       }
     }
