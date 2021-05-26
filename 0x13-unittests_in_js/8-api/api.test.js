17d150ef612261a8ff686b14123a9b6b28e49023
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-05-26 16:29:41 -0500

    [Fix]: Tests

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -2,12 +2,14 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('Index page', () => {
-	it('Status code and route result', (done) => {
-		request.get('http://localhost:7865', (err, res, body) => {
-			if (err) throw err;
-			expect(res.statusCode).to.equal(200);
-			expect(body).to.equal('Welcome to the payment system');
+	describe('/ endpoint', () => {
+		it('Status code and route result', (done) => {
+			request('http://localhost:7865', 'GET', (err, res, body) => {
+				if (err) throw err;
+				expect(res.statusCode).to.equal(200);
+				expect(body).to.equal('Welcome to the payment system');
+				done();
+			});
 		});
-		done();
 	});
 });
