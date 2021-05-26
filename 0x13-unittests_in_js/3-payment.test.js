27d7b04f75ad1922db11e1c626802c24c3b8dea2
BrianFs04 <brayanflorezsanabria@gmail.com>
2021-05-26 13:30:37 -0500

    [Feature]: Spies

---
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+	let consoleSpy;
+	let functionSpy;
+
+	beforeEach(() => {
+		consoleSpy = sinon.spy(console, 'log');
+		functionSpy = sinon.spy(Utils, 'calculateNumber');
+	});
+
+	afterEach(() => {
+		consoleSpy.restore();
+		functionSpy.restore();
+	});
+
+	it('asdsad', () => {
+		sendPaymentRequestToApi(100, 20);
+		expect(functionSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+		expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+	});
+});
